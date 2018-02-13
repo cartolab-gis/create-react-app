c8a84a654086507b3ec6cd8e21b0e276e53670ce
Bates <bates@cartolab.com>
2018-02-12 20:19:12 -0500

    first changes needed to webpack config for boundless sdk apps to build

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -170,7 +170,7 @@ module.exports = {
           // Process JS with Babel.
           {
             test: /\.(js|jsx|mjs)$/,
-            include: paths.appSrc,
+            include: [paths.appSrc, path.resolve(__dirname, '../node_modules/')],
             loader: require.resolve('babel-loader'),
             options: {
               // @remove-on-eject-begin
